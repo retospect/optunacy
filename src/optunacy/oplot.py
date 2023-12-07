fe76e86f6be897baf64124edf2097c2476f6260f
Reto Stamm <reto@retostamm.com>
2023-12-07 12:01:19 -0800

    fixed indent

--- src/optunacy/oplot.py
+++ src/optunacy/oplot.py
@@ -4,15 +4,16 @@ import matplotlib.ticker as ticker
 import numpy as np
 import scipy as scipy
 
+
 class OPlot:
     def __init__(self, study, objective_names=[]):
-        ''' Initialize class with study.
+        """Initialize class with study.
         If objective_names is empty, no objectives can be shown.
         :param study: Optuna study object
-        :param objective_names: List of objective names to show, 
+        :param objective_names: List of objective names to show,
                                 the string names of the objectives i
                                 returned from the objective function
-        '''
+        """
         self.study = study
         self.objective_names = objective_names
 
@@ -26,8 +27,10 @@ class OPlot:
                 if name in combined_dict:
                     values.append(combined_dict[name])
                 else:
-                    raise ValueError(f"Parameter '{name}' not found in trial parameters or user attributes.")
-        return values 
+                    raise ValueError(
+                        f"Parameter '{name}' not found in trial parameters or user attributes."
+                    )
+        return values
 
     def describe_trials(trials):
         i = 0
@@ -44,12 +47,16 @@ class OPlot:
             results.append(desc)
         return results
 
-
     def doIt(self):
         print("testing")
 
+
 def plot(x_name, y_name, z_name=None, x_range=None, y_range=None):
-    trials = [trial for trial in study.trials if trial.state == optuna.trial.TrialState.COMPLETE]
+    trials = [
+        trial
+        for trial in study.trials
+        if trial.state == optuna.trial.TrialState.COMPLETE
+    ]
     x_values = self.get_values(x_name, trials)
     y_values = self.get_values(y_name, trials)
     z_values = self.get_values(z_name, trials) if z_name else None
@@ -62,31 +69,28 @@ def plot(x_name, y_name, z_name=None, x_range=None, y_range=None):
         xi = np.linspace(min(x_values), max(x_values), 100)
         yi = np.linspace(min(y_values), max(y_values), 100)
         X, Y = np.meshgrid(xi, yi)
-        Z = scipy.interpolate.griddata((x_values, y_values), z_values, (X, Y), method='cubic')
+        Z = scipy.interpolate.griddata(
+            (x_values, y_values), z_values, (X, Y), method="cubic"
+        )
 
         # Create contour plot
-        contour = go.Contour(
-            x=xi,
-            y=yi,
-            z=Z,
-            colorscale='Viridis'
-        )
+        contour = go.Contour(x=xi, y=yi, z=Z, colorscale="Viridis")
 
         # Create scatter plot with mouseovers for data points
         scatter = go.Scatter(
             x=x_values,
             y=y_values,
-            mode='markers',
-            marker=dict(color='black', size=5),
+            mode="markers",
+            marker=dict(color="black", size=5),
             text=descriptions,
-            hoverinfo="x+y+z+text"
+            hoverinfo="x+y+z+text",
         )
-        
+
         layout = go.Layout(
             title=f"{x_name} vs {y_name}" + (f" with {z_name}" if z_name else ""),
             xaxis=dict(title=x_name, range=x_range),
             yaxis=dict(title=y_name, range=y_range),
-            hovermode='closest',  # Configure hover mode
+            hovermode="closest",  # Configure hover mode
             annotations=[
                 dict(
                     text=z_name,  # Text you want to display
@@ -94,10 +98,10 @@ def plot(x_name, y_name, z_name=None, x_range=None, y_range=None):
                     xref="paper",  # Use 'paper' for relative positioning
                     yref="paper",
                     x=1.03,  # X position (just right of the colorbar)
-                    y=0.5,   # Y position (center vertically)
-                    textangle=-90  # Angle of the text (vertical)
+                    y=0.5,  # Y position (center vertically)
+                    textangle=-90,  # Angle of the text (vertical)
                 )
-            ]
+            ],
         )
 
         data = [contour, scatter]
@@ -106,10 +110,10 @@ def plot(x_name, y_name, z_name=None, x_range=None, y_range=None):
         scatter = go.Scatter(
             x=x_values,
             y=y_values,
-            mode='markers',
-            marker=dict(color='blue'),
-            text=descriptions, # Mouseover descriptions for each point
-            hoverinfo="x+y+z+text"
+            mode="markers",
+            marker=dict(color="blue"),
+            text=descriptions,  # Mouseover descriptions for each point
+            hoverinfo="x+y+z+text",
         )
 
         data = [scatter]
@@ -118,9 +122,9 @@ def plot(x_name, y_name, z_name=None, x_range=None, y_range=None):
         layout = go.Layout(
             title=f"{x_name} vs {y_name}" + (f" with {z_name}" if z_name else ""),
             xaxis=dict(title=x_name, range=x_range),
-                yaxis=dict(title=y_name, range=y_range),
-                hovermode='closest'  # Configure hover mode
-            )
+            yaxis=dict(title=y_name, range=y_range),
+            hovermode="closest",  # Configure hover mode
+        )
 
         # Create figure and add data
         fig = go.Figure(data=data, layout=layout)
